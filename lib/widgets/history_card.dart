c1e587b792f33467c9a535740000c2e577bb90d1
Learde <leardeleo@gmail.com>
2021-02-21 19:11:10 +0300

    Add new page and new widget

---
+++ lib/widgets/history_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HistoryCard extends StatelessWidget {
+  final String _imgurl;
+  final String _heading;
+  final double _stars;
+  final int _countReviews;
+
+  HistoryCard(this._imgurl, this._heading, this._stars, this._countReviews);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Row(children: [
+        new Image.network(
+          _imgurl,
+          width: 100,
+          height: 100,
+          ),
+        new Column(children: [
+          new Text(_heading),
+          new Row(children: [new Text(_stars.toString()), Icon(Icons.star)],),
+          new Text(_countReviews.toString() + ' отзывов'),
+          ])
+      ],),
+    );
+  }
+}
