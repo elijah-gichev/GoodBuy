2fca260a981da90f0c97833983f79e4cec0130b3
Learde <leardeleo@gmail.com>
2021-02-23 21:03:43 +0300

    Style history page

--- lib/widgets/history_card.dart
+++ lib/widgets/history_card.dart
@@ -11,18 +11,46 @@ class HistoryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: new Row(children: [
-        new Image.network(
-          _imgurl,
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover
+      padding: EdgeInsets.fromLTRB(25, 15, 25, 10),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color.fromRGBO(189, 189, 189, 1)
+          )
+        )
+      ),
+      child: Row(children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Color.fromRGBO(39, 174, 96, 1)),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
           ),
-        new Column(children: [
-          new Text(_heading),
-          new Row(children: [new Text(_stars.toString()), Icon(Icons.star)],),
-          new Text(_countReviews.toString() + ' отзывов'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            child:  Image.network(
+              _imgurl,
+              width: 76,
+              height: 76,
+              fit: BoxFit.cover,
+            ),
+          )
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 23),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 5),
+              child: Text(_heading, style: TextStyle(fontSize: 20)),
+            ),
+            Row(
+              children: [
+              Text(_stars.toString(), style: TextStyle(fontSize: 18)), 
+              Icon(Icons.star)],),
+            Text(_countReviews.toString() + ' отзывов', style: TextStyle(fontSize: 12, color: Color.fromRGBO(189, 189, 189, 1))),
           ])
+        )
       ],),
     );
   }
